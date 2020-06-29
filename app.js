e53637b0e772ff51ed95a76082edbb8a6945c53e
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-29 17:28:06 +0530

    serving static files

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 const http = require("http");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 //parse body automaticaly
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+//this always run
 app.use("/", (req, res, next) => {
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' ><button type='submit'>SEnd</button></form>"
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3001);
